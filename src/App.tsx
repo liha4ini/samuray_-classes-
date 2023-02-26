b2ed23434892416a31d9eb89d0eefcd4900636ed
liha4ini <liha4ev.sergey@gmail.com>
2023-02-26 19:57:45 +0300

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            Hello, samurai! Let's go!
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://png.pngtree.com/png-clipart/20220208/ourmid/pngtree-3d-icon-of-rocket-for-startup-business-and-bitcoins-advertise-png-image_4381459.png" alt=""/>
+            </header>
+            <nav className="nav">
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Messages
+                </div>
+            </nav>
+            <div className="content">
+                Main content
+            </div>
         </div>
     );
 }
